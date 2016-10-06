8fb4f5eee67d2bd3646ba24b8be48f0d9920ca3b
Avika Narula <an1520@nyu.edu>
2016-10-06 14:53:09 -0400

    changed directory

--- javascripts/311_noise.js
+++ javascripts/311_noise.js
@@ -3,8 +3,8 @@ var hour_of_the_day = ["12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "
 "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", 
 "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM"];
 
-d3.json("../data/manhattan.geojson", function(err, geo){
-	d3.csv("../data/noise_filter.csv", function(err_data, data){
+d3.json("manhattan.geojson", function(err, geo){
+	d3.csv("noise_filter.csv", function(err_data, data){
 		main(geo, data);
 	});
 });
